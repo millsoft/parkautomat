0da986c65eb8ff5bf1160e7d69b0b5f2b8cadbd9
Michael Milawski <mm@millsoft.de>
2024-11-30 18:19:22 +0100

    test: add mocking test

--- tests/EnterGateTest.php
+++ tests/EnterGateTest.php
@@ -2,7 +2,10 @@
 declare(strict_types=1);
 
 use M\Parkautomat\Entities\EnterGate;
+use M\Parkautomat\Entities\Ticket;
+use M\Parkautomat\Service\TicketFactory;
 use M\Parkautomat\Service\TicketRegistry;
+use PHPUnit\Framework\MockObject\Rule\InvokedCount;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,6 +25,21 @@ class EnterGateTest extends TestCase
         $ticket2 = $ticketRegistry->get($ticket->getTicketId());
 
         $this->assertSame($ticket, $ticket2);
+
+    }
+
+    public function testMocking()
+    {
+        $enterGate = $this->createMock(EnterGate::class);
+        $ticketBuilder = new TicketFactory();
+        $testTicket = $ticketBuilder->generate();
+        $testTicket->setEnterTime(new DateTimeImmutable());
+
+        $enterGate->expects(new InvokedCount(1))
+            ->method('openGate')->willReturn($testTicket);
+
+        $ticket = $enterGate->openGate();
+        $this->assertNotNull($ticket->getEnterTime());
 
     }
 
